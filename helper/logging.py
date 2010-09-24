433113c74f293e5a924ce7d36f6376526774131c
Ben Pence <bmpence@gmail.com>
2010-09-23 22:05:35 -0400

    Fixed logging. Newline moved to only file writes. Append to file.

--- helper/logging.py
+++ helper/logging.py
@@ -2,25 +2,26 @@
 Logging
 """
 
-
 import datetime
 import os.path
 
 visualCues = True
-
 logFiles = {}
 
-def log(program, messageType = "log", message, visualCue = False):
-	log = "%s\t%s\t%s\t%s:\t%s\n" % (datetime.datetime.now(), messageType, program, message)
+def log(program, message, messageType = "log", visualCue = False):
+	log = "%s\t%s\t%s:\t%s" % (datetime.datetime.now(), messageType, program, message)
 
 	## Output to file ##
 	#Open file if not already open
 
-	if program not in files:
-		logFiles[program] = open("log_" + program, 'w')
+	if program not in logFiles:
+		logFiles[program] = open("log_" + program, 'wa')
 
-	logFiles[program].write(log)
+	logFiles[program].write(log + "\n")
 
 	## Output to screen ##
 	if visualCue and visualCues:
 		print log
+
+log("server", "IT DOES NOT WORK", messageType = "ERR", visualCue = True)
+log("server", "ALL IS WELL", messageType = "LOG", visualCue = True)
