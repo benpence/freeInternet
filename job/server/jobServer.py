f9f2379fa1664a82ffb1f01da7028a8302f7f23f
Ben Pence <bmp@gmail.com>
2010-12-07 16:57:16 -0500

    Improved setup script

--- job/server/jobServer.py
+++ job/server/jobServer.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python
 
-"""
-workISP
-	sends jobs to the clients
-	receives results from the clients
-	verifies results from the clients
-"""
-
-#Add helper libraries
-
 import os
 
 import sys
@@ -16,33 +7,32 @@ sys.path.insert(0, "../../classes")
 import server_class
 
 class ServerJob(server_class.Server):
-	"""
+    """
         TESTTEST
     """
 
     _JOB_DIRECTORY = os.path.join(os.getcwd(), "jobs")
-	
-	def __init__(self):
-        super(Server, self).__init__(jobDirectory=_JOB_DIRECTORY)
+    
+    def __init__(self):
+        super(server_class.Server, self).__init__(jobDirectory=_JOB_DIRECTORY)
 
-		self.listen()
+        self.listen()
 
     def getJobID(self):
         return 0011
 
     def __str__(self):
-        return "workServer"
+        return "jobServer"
 
-	def log(self, *args, **kargs):
-		if 'output' not in kargs:
-			kargs['output'] = self.output
-
-		logging.Logger.log(*args, **kargs)
+    #def log(self, *args, **kargs):
+        #if 'output' not in kargs:
+            #kargs['output'] = self.output
+#
+        #logging.Logger.log(*args, **kargs)
 
 def Main():
-    server = Server(jobDirectory=_JOB_DIRECTORY)
+    server = ServerJob()
     server.listen()
-	server = WorkServer()
 
 if __name__ == "__main__":
-	Main()
+    Main()
