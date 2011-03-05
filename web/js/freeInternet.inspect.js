35c89ca109c884c96788d671d471d81a743efb28
Ben Pence <email@benpence.com>
2011-03-05 14:58:20 -0500

    Made some module separation changes.

---
+++ web/js/freeInternet.inspect.js
@@ -0,0 +1,29 @@
+freeInternet.inspect = function(){
+    // Constants
+    var TEXTS = [
+        "Inspect",
+        "Graph"
+    ];
+    var IMAGES = [
+        "img/inspect.png",
+        "img/graph.png"
+    ];
+        
+    var icon = $("#inspect_icon", $(this));
+
+    charts.inspect = !charts.inspect;
+
+    icon.attr(
+        "src",
+        IMAGES[Number(charts.inspect)]
+    );
+
+    var text = $("#inspect_text", $(this));
+    text.text(
+        TEXTS[Number(charts.inspect)]
+    );
+};
+
+$(function(){
+    $("#inspect").click(freeInternet.inspect);
+});
